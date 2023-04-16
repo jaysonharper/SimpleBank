10275cf0f82307bc8a47a70684e2bfe34b32cccb
jaysonharper <89419645+jaysonharper@users.noreply.github.com>
2023-04-16 14:56:50 -0700

    implement initial alldata

--- components/alldata.js
+++ components/alldata.js
@@ -1,10 +1,50 @@
 function AllData() {
-  const ctx = React.useContext(UserContext);
   return (
-    <>
-      <h5>All Data in Store</h5>
-      {JSON.stringify(ctx)}
-      <br />
-    </>
+    <Card
+      header={"All User Data"}
+      body={
+        <>
+          <AllDataTable />
+        </>
+      }
+    />
+  );
+}
+
+function AllDataTable() {
+  const users = React.useContext(UserContext).users;
+  console.log(users);
+  let userHeaders = [];
+  userHeaders.push(
+    <tr>
+      <th>Email</th>
+      <th>Name</th>
+      <th>Password</th>
+      <th>Balance</th>
+    </tr>
+  );
+
+  let userData = [];
+  users.map((item, index) => {
+    const { email, name, password, balance } = item;
+    userData.push(
+      <tr key={index}>
+        <td>{email}</td>
+        <td>{name}</td>
+        <td>{password}</td>
+        <td>{balance}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <table className="table">
+        <tbody>
+          {userHeaders[0]}
+          {userData[0]}
+        </tbody>
+      </table>
+    </div>
   );
 }
